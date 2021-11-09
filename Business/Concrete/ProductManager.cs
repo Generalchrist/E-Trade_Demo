3788cb7d2b5eb319214856c2cabbe8154565589c
Mert patlar <meertpatlar@gmail.com>
2021-11-09 20:27:02 +0300

    end of backend (15th day)

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,9 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -29,6 +32,7 @@ namespace Business.Concrete {
 
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product) {
             IResult result = BusinessRules.Run(
                 CheckIfProductCountOfCategoryCorrect(product.CategoryId),
@@ -45,6 +49,7 @@ namespace Business.Concrete {
             return new Result(true, Messages.ProductAdded);
         }
 
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll() {
             if (DateTime.Now.Hour == 22) {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
@@ -60,6 +65,8 @@ namespace Business.Concrete {
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
+        [CacheAspect]
+        [PerformanceAspect(5)]
         public IDataResult<Product> GetById(int productId) {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
@@ -96,9 +103,21 @@ namespace Business.Concrete {
             }
             return new ErrorResult(Messages.CategoryLimitExcedeed);
         }
-    
-    
-    
+
+        [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
+        public IResult Update(Product product) {
+            var result = _productDal.GetAll(p=>p.CategoryId== product.CategoryId).Count;
+            if (result >= 10 ) {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            throw new NotImplementedException();
+        }
+
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Product product) {
+            return new SuccessResult("yey");
+        }
     }
 }
 
